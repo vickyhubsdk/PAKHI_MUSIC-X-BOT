973a0b4cb7a9c2aa80a2e7391c22a61f60fe5ab5
𝐀ʟᴘʜᴀ 𝐁ᴀʙʏ <168528642+TEAMPURVI@users.noreply.github.com>
2024-06-08 23:48:13 +0530

    Update queue.py

--- PAKHIMUSIC/utils/stream/queue.py
+++ PAKHIMUSIC/utils/stream/queue.py
@@ -1,8 +1,8 @@
 import asyncio
 from typing import Union
 
-from ANNIEMUSIC.misc import db
-from ANNIEMUSIC.utils.formatters import check_duration, seconds_to_min
+from PAKHIMUSIC.misc import db
+from PAKHIMUSIC.utils.formatters import check_duration, seconds_to_min
 from config import autoclean, time_to_seconds
 
 
