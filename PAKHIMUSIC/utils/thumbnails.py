80aa4215abd54ffc6ed4da9c06d1bab8ba6fa641
vicky0604hello <166818922+vicky0604hello@users.noreply.github.com>
2024-06-10 11:41:33 +0530

    PAKHI MUSIC

--- PAKHIMUSIC/utils/thumbnails.py
+++ PAKHIMUSIC/utils/thumbnails.py
@@ -122,7 +122,7 @@ async def get_thumb(videoid):
 
             image4 = ImageDraw.Draw(image2)
             image4.text((10, 10), "PAKHI MUSIC", fill="white", font = font1, align ="left") 
-            image4.text((670, 150), "PLAYING", fill="white", font = font2, stroke_width=2, stroke_fill="white", align ="left") 
+            image4.text((600, 120), "PLAYING SONG REQUESTED BY USER", fill="white", font = font2, stroke_width=2, stroke_fill="white", align ="left") 
 
             # title
             title1 = truncate(title)
