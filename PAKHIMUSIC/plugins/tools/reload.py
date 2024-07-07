4f1b46ab61ea7bb3434de2422a07bb60aa859ce2
vicky0604hello <choudharyji0604e@gmail.com>
2024-07-07 21:48:15 +0530

    PAKHI MUSIC

--- PAKHIMUSIC/plugins/tools/reload.py
+++ PAKHIMUSIC/plugins/tools/reload.py
@@ -118,7 +118,7 @@ async def help(client: Client, message: Message):
              [
                  [
                       InlineKeyboardButton(
-                         "• нαϲкє𝚍 ву  •", url=f"https://t.me/VICKY_CHOUDHARY_1203")
+                         "• нαϲкє𝚍 ву  •", url=f"https://t.me/Emergency_Gamer")
                  ]
             ]
          ),
